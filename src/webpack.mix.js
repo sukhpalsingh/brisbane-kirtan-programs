726ef904807b2edec196e1495ddef4736c61a49b
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-04-24 07:01:33 +1000

    Adding mdboostrap and removing unnecessary files

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,17 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+    'node_modules/mdbootstrap/js/jquery-3.2.1.min.js',
+    'node_modules/mdbootstrap/js/popper.min.js',
+    'node_modules/mdbootstrap/js/bootstrap.min.js',
+    'node_modules/mdbootstrap/js/mdb.min.js',
+], 'public/build/js/app.js');
+
+mix.styles([
+    'node_modules/mdbootstrap/css/bootstrap.min.css',
+    'node_modules/mdbootstrap/css/mdb.min.css',
+], 'public/build/css/app.css');
+
+mix.copyDirectory('node_modules/mdbootstrap/font', 'public/build');
+mix.copyDirectory('node_modules/mdbootstrap/img', 'public/build');
