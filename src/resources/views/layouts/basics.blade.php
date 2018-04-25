25b8cb9f4f24577fb1bee49439111829e7127921
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-04-25 12:18:29 +1000

    Fixing the site theme

--- src/resources/views/layouts/basics.blade.php
+++ src/resources/views/layouts/basics.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" class="full-height">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,51 +9,53 @@
 
         <link href="/build/css/app.css" rel="stylesheet" type="text/css">
     </head>
-    <body>
-        <div class="container">
+    <body class="rgba-blue-grey-slight">
+        <div id="logo-panel" class="container-fluid">
             <nav id="main-nav" class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="/"><strong>Brisbane Kirtan Programs</strong></a>
                 <div class="float-right">
-                    <a href="/login" class="btn btn-primary btn-rounded btn-sm waves-effect waves-light"><strong>Login</strong></a>
+                    <a href="/login" class="btn btn-dark cyan darken-2 btn-rounded btn-sm waves-effect waves-light"><strong>Login</strong></a>
                 </div>
             </nav>
+        </div>
 
-            <nav class="navbar navbar-expand-md navbar-inverse navbar-dark cyan accent-4">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                    <div id="mainmenu" class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav mr-auto">
-                            <li
-                                @if ($tab === 'home')
-                                    class="nav-item active"
-                                @else
-                                    class="nav-item"
-                                @endif
-                            >
-                                <a class="nav-link waves-effect waves-light" href="/">
-                                    Home
-                                    <span class="sr-only">(current)</span>
-                                </a>
-                            </li>
-                            <li
-                                @if ($tab === 'programs')
-                                    class="nav-item active"
-                                @else
-                                    class="nav-item"
-                                @endif
-                            >
-                                <a class="nav-link waves-effect waves-light" href="/programs">
-                                    Programs
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+        <nav class="navbar navbar-expand-md navbar-inverse navbar-dark cyan darken-3">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                 </div>
-            </nav>
+                <div id="mainmenu" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav mr-auto">
+                        <li
+                            @if ($tab === 'home')
+                                class="nav-item active"
+                            @else
+                                class="nav-item"
+                            @endif
+                        >
+                            <a class="nav-link waves-effect waves-light" href="/">
+                                Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li
+                            @if ($tab === 'programs')
+                                class="nav-item active"
+                            @else
+                                class="nav-item"
+                            @endif
+                        >
+                            <a class="nav-link waves-effect waves-light" href="/programs">
+                                Programs
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid mb-3">
             <div class="col-sm-12">
                 @yield('content')
             </div>
--- src/resources/views/programs/list.blade.php
+++ src/resources/views/programs/list.blade.php
@@ -4,33 +4,34 @@
 
 <div class="row">
     <div class="col-md-12">
-        <a class="btn btn-primary float-right" href="/programs/create">Add Program</a>
+        <a class="btn btn-sm btn-dark cyan darken-2 float-right" href="/programs/create">Add Program</a>
     </div>
 </div>
 
 @if ($programs->count() > 0)
 
     @foreach ($programs as $program)
-        <div class="card card-body text-center">
-            <h4 class="card-title">
+        <div class="card text-center">
+            <div class="card-header white-text cyan darken-2">
                 Akhand Kirtan Devaan - {{ $program->start_date }}
-            </h4>
-            <p class="card-text h4">
-                {{ $program->person_name }}
-            </p>
-            <p class="card-text">
-                {{ $program->start_date }}
-            </p>
-            <p class="card-text">
-                {{ $program->start_time }} - {{ $program->end_time }}
-            </p>
-
-            <p class="card-text">
-                {{ $program->house_no }} {{ $program->street }}
-            </p>
-            <p class="card-text">
-                {{ $program->suburb }}, {{ $program->postcode }}, {{ $program->state }}
-            </p>
+            </div>
+            <div class="card-body">
+                <p class="card-title h3">
+                    {{ $program->person_name }}
+                </p>
+                <p class="card-text">
+                    {{ $program->start_date }}
+                </p>
+                <p class="card-text">
+                    {{ $program->start_time }} - {{ $program->end_time }}
+                </p>
+                <p class="card-text">
+                    {{ $program->house_no }} {{ $program->street }}
+                </p>
+                <p class="card-text">
+                    {{ $program->suburb }}, {{ $program->postcode }}, {{ $program->state }}
+                </p>
+            </div>
         </div>
     @endforeach
 
